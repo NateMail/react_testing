8cb02d7b1514e1383d188568d564bbc89c0c88e4
nathanmailhiot <nathanjm12@outlook.com>
2019-10-03 10:28:20 -0400

    integration test

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -4,29 +4,33 @@ import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
-  beforeEach(() => {
-    moxios.install();
-    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
-      status: 200,
-      response: [{ name: 'Fetch #1' }, { name: 'Fetch #2' }]
-    });
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+    status: 200,
+    response: [{ name: 'Fetch #1' }, { name: 'Fetch #2' }]
   });
+});
 
-  afterEach(() => {
-    moxios.uninstall();
-  });
+afterEach(() => {
+  moxios.uninstall();
+});
 
-  // Attempt to render *entire* app
+it('can fetch a list of comments and display them', done => {
   const wrapped = mount(
     <Root>
       <App />
     </Root>
   );
 
-  // find the 'fetchComments button and click it
   wrapped.find('.fetch-comments').simulate('click');
 
-  // Expect to find a list of comments!
-  expect(wrapped.find('li').length).toEqual(2);
+  moxios.wait(() => {
+    wrapped.update();
+
+    expect(wrapped.find('li').length).toEqual(2);
+
+    done();
+    wrapped.unmount();
+  });
 });
