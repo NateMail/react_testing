56a6eb88ac569050df9a17de9b5b103950507fef
nathanmailhiot <nathanjm12@outlook.com>
2019-10-03 10:10:05 -0400

    Add before each and after each.  Test still failing

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
 it('can fetch a list of comments and display them', () => {
+  beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+      status: 200,
+      response: [{ name: 'Fetch #1' }, { name: 'Fetch #2' }]
+    });
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
   // Attempt to render *entire* app
   const wrapped = mount(
     <Root>
@@ -15,5 +28,5 @@ it('can fetch a list of comments and display them', () => {
   wrapped.find('.fetch-comments').simulate('click');
 
   // Expect to find a list of comments!
-  expect(wrapped.find('li').length).toEqual(500);
+  expect(wrapped.find('li').length).toEqual(2);
 });
