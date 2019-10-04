bba441e2a30d5c79248acf1d95191bffa15280a8
nathanmailhiot <nathanjm12@outlook.com>
2019-10-04 10:26:41 -0400

    Add life cycle methods and handlechange

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,20 @@ import * as actions from 'actions';
 class CommentBox extends Component {
   state = { comment: '' };
 
+  componentDidMount() {
+    this.shouldNavigateAway();
+  }
+
+  componentDidUpdate() {
+    this.shouldNavigateAway();
+  }
+
+  shouldNavigateAway() {
+    if (!this.props.auth) {
+      console.log('I need to leave');
+    }
+  }
+
   handleChange = event => {
     this.setState({ comment: event.target.value });
   };
@@ -35,7 +49,11 @@ class CommentBox extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(CommentBox);
