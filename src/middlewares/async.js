71419079f052839c9f1085313f63c884aa6374df
nathanmailhiot <nathanjm12@outlook.com>
2019-10-05 12:07:18 -0400

    add if it does not have a promise to middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,1 +1,9 @@
-export default ({ dispatch }) => next => action => {};
+export default ({ dispatch }) => next => action => {
+  // check to see if the action has a promise on its 'payload'
+  // If it does, then wiat for it to resolve
+  // If it doesn't, then send the action on to the next middleware
+
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+};
